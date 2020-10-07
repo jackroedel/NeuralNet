6dcdb134330b527a9a89b9ae14f4898d5778f86f
jackroedelgit config --global user.email jack.roedel@hotmail.com <jack.roedel@hotmail.com>
2020-10-07 01:09:32 -0700

    Refactored code to make it easier to read and debug

--- NeuralNetMain.cpp
+++ NeuralNetMain.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "Propogation.h"
-#include "Scaffold.h"
-#include "Variables.h"
-#include "Functions.h"
+#include "Network.cpp"
 
 int main()
 {
     //std::ios_base::sync_with_stdio(false);
     //std::cin.tie(NULL);
+
+    FCLayer Layer1(Functions::Linear, 10);
+
+    Network NetOne;
+    NetOne.PushBack(Layer1);
+
+    NetOne.init();
+
+    for (int i = 0; i < Layer1.NeuronCount; i++)
+    {
+        std::cout << NetOne.Net[0].Weights[i];
+    }
 
     return 0;
 }
